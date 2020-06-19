2ffdd0ed066e01e8c8e43f869f72b2f954f4c9fb
allissondantas <allissondantas@gmail.com>
2020-06-19 08:31:16 -0300

    informeJUN19

--- risk_diagrams.py
+++ risk_diagrams.py
@@ -19,7 +19,7 @@ def main():
 
     if argv_1 == 'brasil' or argv_1 == 'recife' or argv_1 == 'alagoas' or argv_1 == 'para':
         brasil = True
-        pt = False
+        pt = True
         last15days = False
 
         dataTable = []
@@ -72,6 +72,7 @@ def main():
 
             except AttributeError:
                 print('Error! Not found file or could not download!')
+        
 
         data = pd.read_excel(filename, sheet_name= sheet_name)
         population = pd.read_excel(filename_population)
@@ -169,118 +170,115 @@ def main():
                     else:
                         a_14_days_solo.append(None)
             
-
             if brasil and pt:
                 save_path = 'reports_pdf/brasil/risk-pt/'+sheet_name+'/'+ last_day + '-' + region[ID]
                 save_path_xlsx = 'reports_pdf/brasil/risk-pt/'+sheet_name+'/'
-
             else:
                 save_path = 'reports_pdf/brasil/risk-en/'+sheet_name+'/'+ last_day + '-' + region[ID]
                 save_path_xlsx = 'reports_pdf/brasil/risk-en/'+sheet_name+'/'
+           
+            #with PdfPages(save_path + '.pdf') as pdf:
+            fig1, ax1 = plt.subplots(sharex=True)
+            if last15days: 
+                ax1.plot(a_14_days_solo,  p_seven, 'ko--', fillstyle='none', linewidth=0.5) #For last 15 days
+            else: 
+                ax1.plot(a_14_days,  p_seven, 'ko--', fillstyle='none', linewidth=0.5)
+            lim = ax1.get_xlim()
+            x = np.ones(int(lim[1]))
+            ax1.plot(x, 'k--', fillstyle='none', linewidth=0.5)
+            ax1.set_ylim(0, 4)
+            ax1.set_xlim(0, int(lim[1]))
 
-            with PdfPages(save_path + '.pdf') as pdf:
-                fig1, ax1 = plt.subplots(sharex=True)
-                if last15days: 
-                    ax1.plot(a_14_days_solo,  p_seven, 'ko--', fillstyle='none', linewidth=0.5) #For last 15 days
-                else: 
-                    ax1.plot(a_14_days,  p_seven, 'ko--', fillstyle='none', linewidth=0.5)
-                lim = ax1.get_xlim()
-                x = np.ones(int(lim[1]))
-                ax1.plot(x, 'k--', fillstyle='none', linewidth=0.5)
-                ax1.set_ylim(0, 4)
-                ax1.set_xlim(0, int(lim[1]))
+            if brasil and pt:
+                ax1.set_ylabel('\u03C1 (média de '+ cases_deaths +' dos últimos 7 dias)')
+                ax1.set_xlabel(ataque_densidade +' por $10^5$ hab. (últimos 14 dias)')
+            else:
+                ax1.set_ylabel('$\u03C1$ (mean of the last 7 days)')
+                ax1.set_xlabel('Attack rate per $10^5$ inh. (last 14 days)')
+            ax1.annotate(first_day,
+                            xy=(a_14_days[day13], p_seven[day13]), xycoords='data',
+                            xytext=(len(x) - abs(len(x) / 2), 3), textcoords='data',
+                            arrowprops=dict(arrowstyle="->",
+                                            connectionstyle="arc3", linewidth=0.4),
+                            )
+            ax1.annotate(last_day,
+                            xy=(a_14_days[len(a_14_days) - 1], p_seven[len(p_seven) - 1]), xycoords='data',
+                            xytext=(len(x) - abs(len(x) / 3), 3.5), textcoords='data',
+                            arrowprops=dict(arrowstyle="->",
+                                            connectionstyle="arc3", linewidth=0.4),
+                            )
 
-                if brasil and pt:
-                    ax1.set_ylabel('\u03C1 (média de '+ cases_deaths +' dos últimos 7 dias)')
-                    ax1.set_xlabel(ataque_densidade +' por $10^5$ hab. (últimos 14 dias)')
-                else:
-                    ax1.set_ylabel('$\u03C1$ (mean of the last 7 days)')
-                    ax1.set_xlabel('Attack rate per $10^5$ inh. (last 14 days)')
-                ax1.annotate(first_day,
-                             xy=(a_14_days[day13], p_seven[day13]), xycoords='data',
-                             xytext=(len(x) - abs(len(x) / 2), 3), textcoords='data',
-                             arrowprops=dict(arrowstyle="->",
-                                             connectionstyle="arc3", linewidth=0.4),
-                             )
-                ax1.annotate(last_day,
-                             xy=(a_14_days[len(a_14_days) - 1], p_seven[len(p_seven) - 1]), xycoords='data',
-                             xytext=(len(x) - abs(len(x) / 3), 3.5), textcoords='data',
-                             arrowprops=dict(arrowstyle="->",
-                                             connectionstyle="arc3", linewidth=0.4),
-                             )
-    
-                if brasil:
-                    bra_title = region[ID] + ' - Brasil'
-                    plt.title(bra_title)
-                else:
-                    plt.title(region[ID])
-               
-                rh = np.arange(0,int(lim[1]),1)
-                ar = np.linspace(0,4,400)
-              
-                RH, AR = np.meshgrid(rh, ar)
-
-                EPG = RH * AR
-
-                for i in range(len(EPG)):
-                    for j in range(len(EPG[i])):
-                        if EPG[i][j] > 100:
-                            EPG[i][j] = 100
-                c = colormap.Colormap()
-                mycmap = c.cmap_linear('green(w3c)', 'yellow', 'red')
-                ax1.pcolorfast([0, int(lim[1])], [0, 4],EPG, cmap=mycmap, alpha=0.6)
-
-
-                if region[ID] == "Pernambuco" or sys.argv[1] == 'recife':
-                   
-                    plt.subplots_adjust(bottom=0.2)
-                    text_annotate = (
-                        "*A zona vermelha representa alto risco de infecção, enquanto a zona verde representa baixo risco.\n Valores calculados baseados na incidência diária de "+ cases_deaths +" e população. "
-                        "IRRD/PE. \n Fonte: SES-PE. Dados atualizados em "+ str(last_day) +".")
-                    
-                    plt.text(0, -1, text_annotate, fontsize=7, wrap=True)
-                
-                ax1.set_aspect('auto')
-                
-
-                #plt.show()
-                #break
-
-                if brasil and pt:
-                    if last15days: 
-                        save_path_img = 'reports_pdf/brasil/risk-pt/'+sheet_name+'/last15days/'+ last_day + '-' + region[ID] + '_last15days.png'
-                        plt.savefig(save_path_img, bbox_inches='tight', dpi=300)
-                    else:
-                        save_path_img = 'reports_pdf/brasil/risk-pt/'+sheet_name+'/'+ last_day + '-' + region[ID] + '.png'
-                        plt.savefig(save_path_img, bbox_inches='tight', dpi=300)
-
-                    if argv_1 == 'brasil' and last15days == False:
-                        siglasEstados = ["AC", "AL", "AP", "AM", "BA", "CE",
-                        "DF", "ES", "GO", "MA", "MT", "MS",
-                        "MG", "PA", "PB", "PR", "PE", "PI", "RJ",
-                        "RN", "RS", "RO", "RR", "SC", "SP", "SE", "TO", "TOTAL"]
-
-                        save_path_img_site = 'reports_pdf/brasil/risk-pt/'+sheet_name+'/IRRD/'+ siglasEstados[ID] + '.png'
-                        plt.savefig(save_path_img_site, bbox_inches='tight', dpi=300)
-                    elif argv_1 == 'recife' and last15days == False:
-                        save_path_img = 'reports_pdf/brasil/risk-pt/'+sheet_name+'/IRRD/PERNAMBUCO/'+ region[ID] + '.png'
-                        plt.savefig(save_path_img, bbox_inches='tight', dpi=300)
-                else:
-                    plt.savefig(save_path + '.png', bbox_inches='tight', dpi=300)
-
-                try:
-                    pdf.savefig(fig1)
-                    plt.close('all')
-                    print(
-                        "\n\nPrediction for the region of " + region[
-                            ID] + " performed successfully!\nPath:" + save_path +'.png')
-                except:
-                    print("An exception occurred")
+            if brasil:
+                bra_title = region[ID] + ' - Brasil'
+                plt.title(bra_title)
+            else:
+                plt.title(region[ID])
             
+            rh = np.arange(0,int(lim[1]),1)
+            ar = np.linspace(0,4,400)
+            
+            RH, AR = np.meshgrid(rh, ar)
+
+            EPG = RH * AR
+
+            for i in range(len(EPG)):
+                for j in range(len(EPG[i])):
+                    if EPG[i][j] > 100:
+                        EPG[i][j] = 100
+            c = colormap.Colormap()
+            mycmap = c.cmap_linear('green(w3c)', 'yellow', 'red')
+            ax1.pcolorfast([0, int(lim[1])], [0, 4],EPG, cmap=mycmap, alpha=0.6)
+
+
+            if region[ID] == "Pernambuco" or sys.argv[1] == 'recife':
+                
+                plt.subplots_adjust(bottom=0.2)
+                text_annotate = (
+                    "*A zona vermelha representa alto risco de infecção, enquanto a zona verde representa baixo risco.\n Valores calculados baseados na incidência diária de "+ cases_deaths +" e população. "
+                    "IRRD/PE. \n Fonte: SES-PE. Dados atualizados em "+ str(last_day) +".")
+                
+                plt.text(0, -1, text_annotate, fontsize=7, wrap=True)
+            
+            ax1.set_aspect('auto')
+            
+
+            #plt.show()
+            #break
+
+            if brasil and pt:
+                if last15days: 
+                    save_path_img = 'reports_pdf/brasil/risk-pt/'+sheet_name+'/last15days/'+ last_day + '-' + region[ID] + '_last15days.png'
+                    plt.savefig(save_path_img, bbox_inches='tight', dpi=300)
+                else:
+                    save_path_img = 'reports_pdf/brasil/risk-pt/'+sheet_name+'/'+ last_day + '-' + region[ID] + '.png'
+                    plt.savefig(save_path_img, bbox_inches='tight', dpi=300)
+
+                if argv_1 == 'brasil' and last15days == False:
+                    siglasEstados = ["AC", "AL", "AP", "AM", "BA", "CE",
+                    "DF", "ES", "GO", "MA", "MT", "MS",
+                    "MG", "PA", "PB", "PR", "PE", "PI", "RJ",
+                    "RN", "RS", "RO", "RR", "SC", "SP", "SE", "TO", "TOTAL"]
+
+                    save_path_img_site = 'reports_pdf/brasil/risk-pt/'+sheet_name+'/IRRD/'+ siglasEstados[ID] + '.png'
+                    plt.savefig(save_path_img_site, bbox_inches='tight', dpi=300)
+                elif argv_1 == 'recife' and last15days == False:
+                    save_path_img = 'reports_pdf/brasil/risk-pt/'+sheet_name+'/IRRD/PERNAMBUCO/'+ region[ID] + '.png'
+                    plt.savefig(save_path_img, bbox_inches='tight', dpi=300)
+            else:
+                plt.savefig(save_path + '.png', bbox_inches='tight', dpi=300)
+
+            #try:
+                #pdf.savefig(fig1)
+                #plt.close('all')
+                #print(
+                #    "\n\nPrediction for the region of " + region[
+                #        ID] + " performed successfully!\nPath:" + save_path +'.png')
+            #except:
+                #print("An exception occurred")
+        
             dataTable.append([region[ID], cumulative_cases[len(cumulative_cases) - 1], new_cases[len(new_cases) - 1], p[len(p) - 1], p_seven[len(p_seven)  - 1], n_14_days[len(n_14_days) - 1], a_14_days[len(a_14_days) - 1], risk[len(risk) - 1], risk_per_10[len(risk_per_10) - 1]])    
             
             for i in range(len(dia)): dataTable_EPG.append([dia[i], region[ID], risk_per_10[i]])
-            #break
     
     df = pd.DataFrame(dataTable, columns=['State', 'Cumulative cases', 'New cases', 'ρ', 'ρ7', 'New cases last 14 days (N14)', 'New cases last 14 days per 105 inhabitants (A14)', 'Risk (N14*ρ7)',  'Risk per 10^5 (A14*ρ7)' ])       
     df_EPG = pd.DataFrame(dataTable_EPG, columns=['DATE', 'CITY', 'EPG']) 
@@ -290,12 +288,10 @@ def main():
     with ExcelWriter(save_path_xlsx + last_day + '_'+ argv_1 + '_report_EPG.xlsx') as writer:
         df_EPG.to_excel(writer, sheet_name='Alt_Urgell')
 
-            
-
 if __name__ == "__main__":
-    #sys.argv.append('brasil')
+    sys.argv.append('brasil')
     #sys.argv.append('recife')
     #sys.argv.append('alagoas')
-    sys.argv.append('para')
+    #sys.argv.append('para')
     sys.argv.append('False') # True -> Deaths False -> Cases
     main()
