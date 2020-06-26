1147206a1bb142ec84a3c10a90298c30b1972562
allissondantas <allissondantas@gmail.com>
2020-06-26 05:41:05 -0300

    update

--- risk_diagrams.py
+++ risk_diagrams.py
@@ -289,8 +289,8 @@ def main():
         df_EPG.to_excel(writer, sheet_name='Alt_Urgell')
 
 if __name__ == "__main__":
-    #sys.argv.append('brasil')
-    sys.argv.append('recife')
+    sys.argv.append('brasil')
+    #sys.argv.append('recife')
     #sys.argv.append('alagoas')
     #sys.argv.append('para')
     sys.argv.append('False') # True -> Deaths False -> Cases
